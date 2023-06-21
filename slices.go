1ad4eff42dbd0b17fcd78d5dce79eb445e3b6b56
Deepak Gudla <44751447+nameisdeepak@users.noreply.github.com>
2023-06-21 17:54:24 +0530

    Update slices.go

--- slices.go
+++ slices.go
@@ -3,17 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var sports = []string{"cricket", "badminton", "tennis"}
-	fmt.Println(sports)
+        var sports = []string{"cricket", "badminton", "tennis"}
+        fmt.Println(sports)
+        
+        //adding elements to the existing slice
+        sports = append(sports, "football", "volleyball", "basketball", "golf", "wrestling")
+        fmt.Println(sports)
+        sports = append(sports[1:5]) //prints 1 2 3 4 
+        fmt.Println(sports)
+        sports = append(sports[2:6])// prints 2 3 4 5
+        fmt.Println(sports)
+        //sports = append(sports[1:5][2:6]) // ?
+        //fmt.Println(sports)
+        fmt.Println("_________________")
 
-	sports = append(sports, "football", "volleyball", "basketball", "golf", "wrestling")
-	fmt.Println(sports)
-	sports = append(sports[1:5])
-	fmt.Println(sports)
-	sports = append(sports[2:6])
-	fmt.Println(sports)
-	//sports = append(sports[1:5][2:6])
-	//fmt.Println(sports)
-	//1:5 - 1 2 3 4
-	//2:6 - 2 3 4 5
+        a := []string{"hello", "gopher"}
+        fmt.Println(a)
+
+        for i,v := range(sports) {
+                fmt.Printf("index %v is storing the value %v\n", i,v)
+        }
 }
