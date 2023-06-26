290e490685f113b81b46b7c5715eed4c641dc37e
Deepak Gudla <44751447+deepakgudla@users.noreply.github.com>
2023-06-26 12:33:46 +0530

    Update pointers.go

--- pointers.go
+++ pointers.go
@@ -8,8 +8,15 @@ func intPtr(c *int) {
 	*c = 13
 }
 
+func slicePtr(m []int) {
+	m[0] = 13
+}
 
-func main () {
+func mapPtr(y map[string]int, z string) {
+	y[z] = 17
+}
+
+func main() {
 	fmt.Println("-----pointers-----")
 	a := 1357
 	var sport string = "cricket"
@@ -26,6 +33,19 @@ func main () {
 
 	//prinitng pass by value..
 	intPtr(&a)
-	fmt.Print("pass by value : ", a)
+	fmt.Println("pass by value : ", a)
+	//slices-pointers.. pass by value
+	fmt.Println(".....slices & pointers.....")
+	n := []int{1, 3, 5, 7}
+	fmt.Println(n)
+	slicePtr(n)
+	fmt.Println(n)
+
+	//maps-pointers - pass by value
+	x := make(map[string]int)
+	x["golang"] = 77
+	fmt.Println(x["golang"])
+	mapPtr(x, "go")
+	fmt.Println(x["go"])
 
 }
