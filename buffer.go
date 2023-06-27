d1defa477e32345796ea34daaaff297db234a4ab
Deepak Gudla <44751447+deepakgudla@users.noreply.github.com>
2023-06-27 16:51:51 +0530

    buffer

---
+++ buffer.go
@@ -0,0 +1,23 @@
+//buffer
+package main 
+
+import (
+	"fmt"
+	"bytes"
+)
+
+func main() {
+	fmt.Println("buffers")
+	a := bytes.NewBufferString("Test")
+	a.WriteString("cricket")
+	fmt.Println(a.String())
+	//can rewrite the string using reset 
+	a.Reset()
+	a.WriteString("badminton")
+	fmt.Println(a.String())
+
+	//writing
+	a.Write([]byte("byte buffer"))
+	fmt.Println(a.String())
+
+}
