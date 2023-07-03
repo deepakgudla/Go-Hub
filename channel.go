e82dde5e47dca57fa23aa8fa0d36a3da11618ed8
deepakgudla <thedeepak1357@gmail.com>
2023-07-03 22:06:20 +0530

    channel

--- channel.go
+++ channel.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("---channels---")
+
+	a := make (chan int)
+
+	go func () {
+		for i:=0;  i<3; i++ {
+			a <-i
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Println(<-a)
+		}
+	} ()
+
+	time.Sleep(time.Minute)
 }
