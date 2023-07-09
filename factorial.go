d36449eb1474721ee87c6e63758be0c151d44a89
deepakgudla <thedeepak1357@gmail.com>
2023-07-09 22:44:05 +0530

    factorial

--- factorial.go
+++ factorial.go
@@ -1,17 +1,37 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	a := factorial(13)
+// 	fmt.Println("Total:", a)
+// }
+
+// func factorial (n int)  int {
+// 	total := 1
+// 	for i:=n; i>0; i-- {
+// 		total *= i
+// 	}
+
+// 	return total
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	a := factorial(13)
-	fmt.Println("Total:", a)
+func factorial(n int) int {
+	if n <= 1 {
+		return 1
+	}
+	return n * factorial(n-1)
 }
 
-func factorial (n int)  int {
-	total := 1
-	for i:=n; i>0; i-- {
-		total *= i
-	}
-
-	return total
+func main() {
+	var num int
+	fmt.Print("Enter a number: ")
+	fmt.Scanln(&num)
+	fmt.Printf("Factorial of %d is %d\n", num, factorial(num))
 }
