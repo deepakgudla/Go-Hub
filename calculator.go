e8fc9753bedd66ee16d8bfa463c9ff1eb3f7a138
deepakgudla <thedeepak1357@gmail.com>
2023-07-16 23:54:03 +0530

    calculator

---
+++ calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+
+	
+	sum := a + b
+	fmt.Printf("Sum: %d\n", sum)
+
+	
+	diff := a - b
+	fmt.Printf("Difference: %d\n", diff)
+
+	
+	product := a * b
+	fmt.Printf("Product: %d\n", product)
+
+	
+	quotient := a / b
+	fmt.Printf("Quotient: %d\n", quotient)
+
+	
+	remainder := a % b
+	fmt.Printf("Remainder: %d\n", remainder)
+}
