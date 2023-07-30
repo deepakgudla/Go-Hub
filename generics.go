7362988d00993b550f75c2d543376734e498d517
deepakgudla <thedeepak1357@gmail.com>
2023-07-30 12:40:29 +0530

    generic

---
+++ generics.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func addA(a, b int) int {
+	return a + b
+}
+
+func addB(a, b float64) float64 {
+	return a + b
+}
+
+func genericG[G int | float64](a, b G) G {  //[G int | float64] --> this is called as generic which accepts either of the parameter
+	return  a + b
+}
+
+func main() {
+
+	fmt.Println(addA(1,3))
+	fmt.Println(addB(1.35, 0.77))
+	fmt.Println("---generic func---")
+	fmt.Println(genericG[int](1, 123))
+	fmt.Println(genericG[float64](1.35, 7.77))
+}
