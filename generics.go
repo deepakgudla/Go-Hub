1e4541a1d8ae53f92572a965d9e38d3da4c95fbd
deepakgudla <thedeepak1357@gmail.com>
2023-07-31 23:08:18 +0530

    type set in generic

--- generics.go
+++ generics.go
@@ -13,6 +13,15 @@ func addB(a, b float64) float64 {
 func genericG[G int | float64](a, b G) G {  //[G int | float64] --> this is called as generic which accepts either of the parameter
 	return  a + b
 }
+//instead of declaring data type in generic, we can do as such
+
+type addNumbers interface {
+	int | float64
+}
+
+func genericG2[G  addNumbers](a, b G) G {  //[G int | float64] --> this is called as generic which accepts either of the parameter
+	return  a + b
+}
 
 func main() {
 
@@ -21,4 +30,6 @@ func main() {
 	fmt.Println("---generic func---")
 	fmt.Println(genericG[int](1, 123))
 	fmt.Println(genericG[float64](1.35, 7.77))
+	fmt.Println("type set....")
+	fmt.Println("addition of numbers using type set interface :",genericG2[int]( 1, 1357))
 }
