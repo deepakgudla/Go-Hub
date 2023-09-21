4202376bd53c488a3865174699876f309debb566
Deepak Gudla <44751447+deepakgudla@users.noreply.github.com>
2023-09-21 12:19:44 +0530

    Update abcd.go

--- Fundamentals/abcd.go
+++ Fundamentals/abcd.go
@@ -14,8 +14,8 @@ import (
 
 func main() {
 	const name = "worldcup"
-	const age = 4
+	const period = 4
 	//unicode := 2693
-	fmt.Printf("%s is played for every %d years.\n", name, age)
+	fmt.Printf("%s is played for every %d years.\n", name, period)
 	//fmt.Println("Unicode number: %u\n", unicode);
 }
