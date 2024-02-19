d66a9ee76a63d56f756738767ff0488576e434ef
deepakgudla <thedeepak1357@gmail.com>
2024-02-19 21:48:19 +0530

    get

--- projects/rest-api/main.go
+++ projects/rest-api/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("api")
-//TODO: CRUD API -> SQL db
+	server := gin.Default() //gin - pre configured server
+
+	server.GET("/employees", getEmployees)
+
+	server.Run(":1357")
+}
+
+func getEmployees(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"text": "Hello World!"})
 }
