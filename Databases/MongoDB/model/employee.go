f0e19a8d3ee3200e130df3f26744f0482dc25e70
deepakgudla <thedeepak1357@gmail.com>
2025-01-07 22:43:13 +0530

    added emp-test

--- Databases/MongoDB/model/employee.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Employee struct {
-	EmployeeID  string `json:"employee_id,omitempty" bson:"employee_id"`
-	Name        string `json:"name,omitempty" bson:"name"`
-	Age         int    `json:"age,omitempty" bson:"age"`
-	Designation string `json:"designation,omitempty" bson:"designation"`
-}
