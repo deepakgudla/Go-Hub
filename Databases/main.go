5ec4dbba2a206a257a3e908a8f030aa9341c332e
deepakgudla <thedeepak1357@gmail.com>
2025-01-10 21:28:37 +0530

    add main func

---
+++ Databases/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var mongoClient *mongo.Client
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env var", err)
+	}
+	log.Println("Env var loaded")
+
+	mongoClient, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal("Database connection failed :( )", err)
+	}
+
+	err = mongoClient.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("ping failed", err)
+	}
+
+	log.Println("Successfully connected to Database :) ")
+
+}
+
+func main() {
+
+	defer mongoClient.Disconnect(context.Background())
+
+	r := mux.NewRouter()
+	r.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
+	log.Println("server is running on port 1357")
+	http.ListenAndServe(":1357", r)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Database running :) "))
+}
