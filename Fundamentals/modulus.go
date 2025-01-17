75f2985749f991a8dea8e981d158f6db5ea2db5a
deepakgudla <thedeepak1357@gmail.com>
2025-01-17 21:30:18 +0530

    lint fix

--- Fundamentals/modulus.go
+++ Fundamentals/modulus.go
@@ -8,10 +8,9 @@ func main() {
 	b := 22 % 10
 	fmt.Println(a, b)
 
-	for i :=0; i <20; i++ {
-		 if i%2 != 0 {
-			 fmt.Printf("odd number is %v\n", i)
-		 }
-	 }
-
+	for i := 0; i < 20; i++ {
+		if i%2 != 0 {
+			fmt.Printf("odd number is %v\n", i)
+		}
+	}
 }
