9f70f04afec203dc023f8ab098137cdc9cf7ecab
deepakgudla <thedeepak1357@gmail.com>
2025-02-03 22:18:40 +0530

    rmdir rest-api

--- projects/rest-api/models/employee.go
+++
@@ -1,9 +0,0 @@
-package models //storing employee data in db
-
-type Employee struct {
-	Name          string
-	Designation   string
-	UserID        int
-	Location      string
-	ContactNumber int
-}
