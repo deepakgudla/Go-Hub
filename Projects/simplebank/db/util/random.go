0f968610df8ee079726200e4e9cd447845f795c7
deepakgudla <thedeepak1357@gmail.com>
2025-11-09 15:59:27 +0530

    add project simplebank

---
+++ Projects/simplebank/db/util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghiklmnopqrstuvwxyz"
+
+// func init() {
+// 	rand.Seed(time.Now().UnixNano())
+// }
+
+var rng *rand.Rand
+
+func init() {
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var ab strings.Builder
+	h := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		d := alphabet[rand.Intn(h)]
+		ab.WriteByte(d)
+	}
+
+	return ab.String()
+}
+
+func RandomOwner() string {
+	return RandomString(7)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "INR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
